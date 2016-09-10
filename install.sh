ffe89d387bf418a9d5372bbfd7bcdd79677f64e6
Gregoire Lejay <gregoire.lejay@gmail.com>
2016-09-10 09:56:18 +0100

    Reorganising the dotfile structure

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DEV_DIR=dev
+
+ln -sf ~/"${DEV_DIR}"/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/"${DEV_DIR}"/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/"${DEV_DIR}"/dotfiles/tmux/.tmux.conf ~/.tmux.conf
