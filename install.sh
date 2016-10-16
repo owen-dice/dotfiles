32dd380be8d078bc13237cc60631710f840f33ba
Gregoire Lejay <gregoire.lejay@gmail.com>
2016-10-16 09:27:18 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ DEV_DIR=dev
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/supercrabtree/k "$HOME"/.oh-my-zsh/custom/plugins/k
 
-ln -sf ~/"${DEV_DIR}"/dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/"${DEV_DIR}"/dotfiles/zsh/.aliases ~/.aliases
-ln -sf ~/"${DEV_DIR}"/dotfiles/zsh/.env ~/.env
+ln -sf "${HOME}"/"${DEV_DIR}"/tools/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf "${HOME}"/"${DEV_DIR}"/tools/dotfiles/zsh/.aliases ~/.aliases
+ln -sf "${HOME}"/"${DEV_DIR}"/tools/dotfiles/zsh/.env ~/.env
 
-ln -sf ~/"${DEV_DIR}"/dotfiles/vim/.vimrc ~/.vimrc
-ln -sf ~/"${DEV_DIR}"/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf "${HOME}"/"${DEV_DIR}"/tools/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf "${HOME}"/"${DEV_DIR}"/tools/dotfiles/tmux/.tmux.conf ~/.tmux.conf
